228472046fde93fe8dd8b479db82b5acc6553b52
unknown <b2212140@photon.chitose.ac.jp>
2022-11-02 11:53:40 +0900

    クラス作成

--- untitled/src/lecture01/kadai12.java
+++ untitled/src/lecture01/kadai12.java
@@ -1,4 +1,8 @@
 package lecture01;
 
 public class kadai12 {
+    public static void main(String[] args) {
+        int studentNumber = 2212140;
+        System.out.println("B"+studentNumber+"Haruaki Horisawa");
+    }
 }
