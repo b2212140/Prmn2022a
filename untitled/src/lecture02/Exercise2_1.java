e90b948d588cce5c968a21e9f3c4c91a140288e7
unknown <b2212140@photon.chitose.ac.jp>
2022-11-09 02:33:57 +0900

    クラス作成

--- untitled/src/lecture02/Exercise2_1.java
+++ untitled/src/lecture02/Exercise2_1.java
@@ -2,10 +2,10 @@ package lecture02;
 
 public class Exercise2_1 {
     public static void main(String[] args) {
-        Human human1 =new Human("haruaki",20);
+        Human human1 =new Human("たかし",18);
         human1.print();
 
-        Human human2 = new Human("siro",5);
+        Human human2 = new Human("ひろし",20);
         human1.print();
         human2.print();
     }
