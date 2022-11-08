575a5e4a27f694b6a827b776a92b0359c95a905f
unknown <b2212140@photon.chitose.ac.jp>
2022-11-09 02:29:40 +0900

    クラス作成

--- untitled/src/lecture02/Exercise2_1.java
+++ untitled/src/lecture02/Exercise2_1.java
@@ -1,4 +1,12 @@
 package lecture02;
 
 public class Exercise2_1 {
+    public static void main(String[] args) {
+        Human human1 =new Human("haruaki",20);
+        human1.print();
+
+        Human human2 = new Human("siro",5);
+        human1.print();
+        human2.print();
+    }
 }
--- untitled/src/lecture02/Human.java
+++ untitled/src/lecture02/Human.java
@@ -1,12 +1,24 @@
 package lecture02;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Human {
     String name;
     int age;
     Human(String name , int age){
         this.name = name;
         this.age = age;
+    }
+
+    void print(){
+        String student=" ";
+        if(age<=18){
+            student="生徒";
+        } else if (age<=22) {
+            student="学生";
+        }
 
 
+        System.out.println(student+":"+name+" "+age+"歳です。");
     }
 }
