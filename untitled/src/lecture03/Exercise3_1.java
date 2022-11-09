1cd6a0c0ea83f732a29885955cb9dd49b702c56f
unknown <b2212140@photon.chitose.ac.jp>
2022-11-09 12:07:14 +0900

    クラス作成

--- untitled/src/lecture03/Exercise3_1.java
+++ untitled/src/lecture03/Exercise3_1.java
@@ -1,4 +1,12 @@
 package lecture03;
 
+import java.util.Scanner;
+
 public class Exercise3_1 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("任意の文字列を入力してください:");
+        String pass=input.nextLine();
+        System.out.println(pass+"と入力されました");
+    }
 }
