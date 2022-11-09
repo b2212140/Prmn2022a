393253180f60749cf8997e862c2bc4d7654e5553
unknown <b2212140@photon.chitose.ac.jp>
2022-11-09 11:43:31 +0900

    不要なファイルを削除した

--- prmn2022a/prmn2022A/src/lecture01/kadai12.java
+++
@@ -1,8 +0,0 @@
-package lecture01;
-
-public class kadai12 {
-    public static void main(String[] args) {
-        int studentNumber = 2212140;
-        System.out.println("B"+studentNumber+"Haruaki Horisawa");
-    }
-}
