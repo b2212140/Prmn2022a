3039ac9f0d7205714ac8c87c82f6dabb327d231c
unknown <b2212140@photon.chitose.ac.jp>
2022-11-09 12:18:19 +0900

    クラス作成

--- untitled/src/lecture03/Exercise3_2.java
+++ untitled/src/lecture03/Exercise3_2.java
@@ -1,4 +1,12 @@
 package lecture03;
 
+import java.util.Scanner;
+
 public class Exercise3_2 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("何行分入力しますか？");
+        int line= input.nextInt();
+    }
+
 }
