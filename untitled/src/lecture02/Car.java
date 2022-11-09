76bbb3a3ee80aac79ee94b1b14a3312efd798b6e
unknown <b2212140@photon.chitose.ac.jp>
2022-11-09 11:03:13 +0900

    クラス作成

--- untitled/src/lecture02/Car.java
+++ untitled/src/lecture02/Car.java
@@ -1,4 +1,17 @@
 package lecture02;
 
 public class Car {
+    int fuel;
+    Car(){
+        this.fuel=0;
+    }
+
+    void run(){
+        if(fuel>=1){
+            fuel=fuel-1;
+            System.out.println("燃料を1消費して走りました。");
+        }else{
+            System.out.println("燃料が足りないため走れませんでした。");
+        }
+    }
 }
