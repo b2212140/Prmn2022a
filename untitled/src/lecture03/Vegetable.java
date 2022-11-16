006f2fdb6d1bdd09ffbadede2fb5ccf5395465e6
unknown <b2212140@photon.chitose.ac.jp>
2022-11-16 12:04:34 +0900

    クラス作成

--- untitled/src/lecture03/Vegetable.java
+++ untitled/src/lecture03/Vegetable.java
@@ -1,4 +1,16 @@
 package lecture03;
 
+import java.util.Scanner;
+
 public class Vegetable {
+    String name;
+    int value;
+    Vegetable(String name,int value){
+        this.name = name;
+        this.value = value;
+    }
+    Scanner input = new Scanner(System.in);
+    void print(){
+        System.out.println(name+"は"+value+"円");
+    }
 }
