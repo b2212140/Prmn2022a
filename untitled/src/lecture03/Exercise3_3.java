4d422ee46361b8399c849e6fc848df44dc038274
unknown <b2212140@photon.chitose.ac.jp>
2022-11-16 11:55:08 +0900

    クラス作成

--- untitled/src/lecture03/Exercise3_3.java
+++ untitled/src/lecture03/Exercise3_3.java
@@ -7,14 +7,15 @@ public class Exercise3_3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int[] num;
-        ArrayList<String> numbers = new ArrayList<>();
+        int[] num = new int[2];
+        ArrayList<String> strings = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
         for(int i=0;i<2;i++){
             System.out.println((i+1)+"つ目の整数を入力してください:");
-            numbers.add(input.nextLine());
-
+            strings.add(input.nextLine());
+            num[i]= Integer.parseInt(strings.get(i));
         }
-
+        System.out.println(num[0]+"+"+num[1]+"="+(num[0]+num[1]));
 
     }
 }
