fcd084669afb3b43f07c4d5f62dc2c78f85f4a8b
unknown <b2212140@photon.chitose.ac.jp>
2022-12-07 10:51:11 +0900

    5-6クラス作成

---
+++ untitled/src/lecture05/Exercise5_6.java
@@ -0,0 +1,8 @@
+package lecture05;
+
+public class Exercise5_6 {
+    public static void main(String[] args) {
+        SwallowtailButterfly swallowtailButterfly = new SwallowtailButterfly();
+        swallowtailButterfly.move();
+    }
+}
