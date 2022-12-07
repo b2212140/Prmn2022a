0be38ff296af96b2b1ab277f8987b81611085466
unknown <b2212140@photon.chitose.ac.jp>
2022-12-07 12:04:17 +0900

    クラス作成

--- untitled/src/lecture07/Skill.java
+++ untitled/src/lecture07/Skill.java
@@ -1,4 +1,14 @@
 package lecture07;
 
 public class Skill {
+    private String name;
+    private int power;
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPower() {
+        return power;
+    }
 }
